f8ad260c4b9741f37ce62ed4b7eadae6dc4dee1b
Alla_Makhotka <all.mahotka2012@yandex.ru>
2021-09-08 22:53:22 +0300

    first homework

---
+++ task01.py
@@ -0,0 +1,16 @@
+'''a = 5
+b = 10
+if a > b:
+    print('a > b')
+elif a < b:
+    print('a < b')
+else:
+    print ('a == b')'''
+
+
+list_ = []
+i = 0
+while i < 50:
+    list_.append(i)
+    i = i + 2
+print (list_)
