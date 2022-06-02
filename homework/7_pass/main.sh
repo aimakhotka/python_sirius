02856026322f809e7c4037ff80e55951faf98ec2
Alla Makhotka <all.mahotka2012@yandex.ru>
2022-06-02 14:11:13 +0300

    upd 7 hw

--- homework/7_pass/main.sh
+++ homework/7_pass/main.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-rm new_test
+
+echo -n 'Введите имя файла: '; read file
+
+if [ -f new_$file ]; then rm new_$file; fi
 
 while read line
 do
-    echo "$line" | cut -d " " -f 1 >> new_test
-    echo "$line" | cut -d " " -f 2 | md5sum | sed 's/  -//g'>> new_test
-done < test
+    echo "$line" | cut -d " " -f 1 >> new_$file
+    echo "$line" | cut -d " " -f 2 | md5sum | sed 's/  -//g'>> new_$file
+done < $file
